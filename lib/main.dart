6d31477986a82d4581f6ec8ecb67730fb7f980e5
Saksham Mathur <sammathur4@gmail.com>
2023-01-29 14:57:00 +0530

    day10

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:project1/screens/homeScreen.dart';
 import 'package:project1/screens/loginScreen.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:project1/utils/routes.dart';
+import 'package:project1/widget/themes.dart';
+
 
 
 void main() {
@@ -17,13 +19,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       // home: HomePage()
-      theme: ThemeData(
-          primarySwatch: Colors.deepPurple,
-          primaryTextTheme: GoogleFonts.latoTextTheme(),
-      ),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark
-      ),
+      theme: MyTheme.darkTheme(context) ,
+      darkTheme: MyTheme.darkTheme(context)  ,
       debugShowCheckedModeBanner: false,
       initialRoute: MyRoute.homeRoute,
       routes: {
--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -7,7 +7,12 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Title"),
+        title: Text("App"),
+        // backgroundColor: Colors.white,
+        // elevation: 0.0,
+        // iconTheme: IconThemeData(color: Colors.black),
+        // title: Text("Title", style: TextStyle(color: Colors.black),),
+
       ),
       body: Center(
         child:  Container(
