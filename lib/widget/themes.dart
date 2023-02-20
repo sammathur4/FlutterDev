a64683761ec56ffc2caa618e02fa10be899ab12d
Saksham Mathur <sammathur4@gmail.com>
2023-02-20 23:32:04 +0530

    Day 17

--- lib/widget/themes.dart
+++ lib/widget/themes.dart
@@ -1,30 +1,53 @@
+// import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
+//
+//
+// class MyTheme {
+//   static ThemeData lightTheme(BuildContext context) =>ThemeData(
+//       primarySwatch: Colors.green,
+//       primaryTextTheme: GoogleFonts.latoTextTheme(),
+//       appBarTheme: AppBarTheme(
+//         color: Colors.black,
+//         elevation: 0.0,
+//         iconTheme: IconThemeData(color: Colors.black ),
+//         toolbarTextStyle: Theme.of(context).textTheme.bodyText2,
+//         titleTextStyle: Theme.of(context).textTheme.headline6  ,
+//       )
+//   );
+//
+//   static ThemeData darkTheme(BuildContext context) =>ThemeData(
+//       primarySwatch: Colors.brown,
+//       primaryTextTheme: GoogleFonts.latoTextTheme(),
+//       appBarTheme: AppBarTheme(
+//         color: Colors.white,
+//         elevation: 0.0,
+//         iconTheme: IconThemeData(color: Colors.black ),
+//         toolbarTextStyle: Theme.of(context).textTheme.bodyText2,
+//         titleTextStyle: Theme.of(context).textTheme.headline6  ,
+//       )
+//   );
+// }
+//
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-
 class MyTheme {
-  static ThemeData lightTheme(BuildContext context) =>ThemeData(
-      primarySwatch: Colors.green,
-      primaryTextTheme: GoogleFonts.latoTextTheme(),
-      appBarTheme: AppBarTheme(
-        color: Colors.black,
-        elevation: 0.0,
-        iconTheme: IconThemeData(color: Colors.black ),
-        toolbarTextStyle: Theme.of(context).textTheme.bodyText2,
-        titleTextStyle: Theme.of(context).textTheme.headline6  ,
-      )
-  );
-
-  static ThemeData darkTheme(BuildContext context) =>ThemeData(
-      primarySwatch: Colors.brown,
-      primaryTextTheme: GoogleFonts.latoTextTheme(),
+  static ThemeData lightTheme(BuildContext context) => ThemeData(
+      primarySwatch: Colors.deepPurple,
+      fontFamily: GoogleFonts.poppins().fontFamily,
       appBarTheme: AppBarTheme(
         color: Colors.white,
         elevation: 0.0,
-        iconTheme: IconThemeData(color: Colors.black ),
-        toolbarTextStyle: Theme.of(context).textTheme.bodyText2,
-        titleTextStyle: Theme.of(context).textTheme.headline6  ,
-      )
-  );
-}
+        iconTheme: IconThemeData(color: Colors.black),
+        textTheme: Theme.of(context).textTheme,
+      ));
 
+  static ThemeData darkTheme(BuildContext context) => ThemeData(
+    brightness: Brightness.dark,
+  );
+
+  //Colors
+  static Color creamColor = Color(0xfff5f5f5);
+  static Color darkBluishColor = Color(0xff403b58);
+}
